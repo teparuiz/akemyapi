776a741677a8df8899e43e15757631bee373fb9a
teparuiz <rodrigo.j.ruiz.us@gmail.com>
2024-02-01 15:29:37 -0600

    protect routes

--- src/cash-cut/cash-cut.controller.ts
+++ src/cash-cut/cash-cut.controller.ts
@@ -6,36 +6,42 @@ import {
   Patch,
   Param,
   Delete,
+  UseGuards,
 } from '@nestjs/common';
 import { CashCutService } from './cash-cut.service';
 import { CreateCashCutDto } from './dto/create-cash-cut.dto';
 import { UpdateCashCutDto } from './dto/update-cash-cut.dto';
-
+import { AuthGuard } from './../auth/auth.guard';
 @Controller('cash-cut')
 export class CashCutController {
   constructor(private readonly cashCutService: CashCutService) {}
 
   @Post()
+  @UseGuards(AuthGuard)
   create(@Body() createCashCutDto: CreateCashCutDto) {
     return this.cashCutService.create(createCashCutDto);
   }
 
   @Get()
+  @UseGuards(AuthGuard)
   findAll() {
     return this.cashCutService.findAll();
   }
 
   @Get(':id')
+  @UseGuards(AuthGuard)
   findOne(@Param('id') id: number) {
     return this.cashCutService.findOne(id);
   }
 
   @Patch(':id')
+  @UseGuards(AuthGuard)
   update(@Param('id') id: number, @Body() updateCashCutDto: UpdateCashCutDto) {
     return this.cashCutService.update(id, updateCashCutDto);
   }
 
   @Delete(':id')
+  @UseGuards(AuthGuard)
   remove(@Param('id') id: number) {
     return this.cashCutService.remove(id);
   }
