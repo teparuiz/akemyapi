95ec973ff2eb4e6b86ae2d362e78f1a5c67ccf46
teparuiz <rodrigo.j.ruiz.us@gmail.com>
2024-02-01 15:12:02 -0600

    añadiendo autorización token en proveedores y usuarios

--- src/providers/providers.controller.ts
+++ src/providers/providers.controller.ts
@@ -6,31 +6,36 @@ import {
   Patch,
   Param,
   Delete,
+  UseGuards,
 } from '@nestjs/common';
 import { ProvidersService } from './providers.service';
 import { CreateProviderDto } from './dto/create-provider.dto';
 import { UpdateProviderDto } from './dto/update-provider.dto';
-
+import { AuthGuard } from './../auth/auth.guard';
 @Controller('providers')
 export class ProvidersController {
   constructor(private readonly providersService: ProvidersService) {}
 
   @Post()
+  @UseGuards(AuthGuard)
   create(@Body() createProviderDto: CreateProviderDto) {
     return this.providersService.create(createProviderDto);
   }
 
   @Get()
+  @UseGuards(AuthGuard)
   findAll() {
     return this.providersService.findAll();
   }
 
   @Get(':id')
+  @UseGuards(AuthGuard)
   findOne(@Param('id') id: number) {
     return this.providersService.findOne(id);
   }
 
   @Patch(':id')
+  @UseGuards(AuthGuard)
   update(
     @Param('id') id: number,
     @Body() updateProviderDto: UpdateProviderDto,
@@ -39,6 +44,7 @@ export class ProvidersController {
   }
 
   @Delete(':id')
+  @UseGuards(AuthGuard)
   remove(@Param('id') id: number) {
     return this.providersService.remove(id);
   }
