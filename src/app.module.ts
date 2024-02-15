741698065a94ec3d743eabe0f10c4151b3860881
teparuiz <rodrigo.j.ruiz.us@gmail.com>
2024-02-15 17:44:15 -0600

    add variables de enterno

--- src/app.module.ts
+++ src/app.module.ts
@@ -14,7 +14,7 @@ import { StoreModule } from './store/store.module';
     TypeOrmModule.forRoot({
       type: 'mysql',
       host: process.env.DB_HOST,
-      port: process.env.DB_PORT,
+      port: 39083,
       username: process.env.DB_USER,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_DATABASE,
