3cb3c090d9a2e89e23200b8adb10ffbe74259a9d
teparuiz <rodrigo.j.ruiz.us@gmail.com>
2024-02-15 17:04:56 -0600

    cambios en variables producción

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,8 @@ import { AppModule } from './app.module';
 declare const module: any;
 async function boostrap() {
   const app = await NestFactory.create(AppModule);
+
+  const port = process.env.PORT || 3000;
   app.setGlobalPrefix('api/v1');
   app.enableCors();
 
@@ -14,7 +16,7 @@ async function boostrap() {
       transform: true,
     }),
   );
-  await app.listen(8000 || 3000);
+  await app.listen(port, '0.0.0.0');
 
   if (module.hot) {
     module.hot.accept();
